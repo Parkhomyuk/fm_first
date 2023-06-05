953c2999a0b6c2bb2ad1704c0da9723069981e51
alex p <alexp.p4b@gmail.com>
2023-06-05 13:37:11 +0300

    fix second commit

--- main_script.js
+++ main_script.js
@@ -3,10 +3,15 @@
     fetch('data.json').then(response=>{
         return response.json()
     }).then(json=>{
+        score=0
     for (let i=0; i<json.length;i++){       
-        document.getElementById(`${json[i]["category"]}-value`).innerHTML=json[i]['score']+' / 100'
+        document.getElementById(`${json[i]["category"]}-value`).innerHTML=json[i]['score'] 
         document.getElementById(`${json[i]["category"]}-img`).src=json[i]['icon']
+        score+=json[i]['score']
     }
+    document.getElementById('score-value').innerHTML=Math.floor(score/json.length)
+    
+
     });
     
    
